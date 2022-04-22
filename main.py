81ab5eba931423adf7d87867a35988e3efa85991
cepheus11 <cepheus11@nym.hush.com>
2022-04-22 21:14:23 +0200

    Added a loop to play multiple times.

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
 from morse_dict import MORSE_CODE
 
-input_text = input("Enter text to translate into morse code: ").lower()
-output_code = ''
-omitted_count = 0
-for c in input_text:
-    if MORSE_CODE.__contains__(c):
-        output_code += MORSE_CODE[c]
-    else:
-        omitted_count += 1
-    output_code += ' '
-print(f"The text in Morse Code is: {output_code}")
-if omitted_count > 0:
-    print(f"{omitted_count} characters were omitted.")
+cont = True
+while cont:
+    input_text = input("Enter text to translate into morse code: ").lower()
+    output_code = ''
+    omitted_count = 0
+    for c in input_text:
+        if MORSE_CODE.__contains__(c):
+            output_code += MORSE_CODE[c]
+        else:
+            omitted_count += 1
+        output_code += ' '
+    print(f"The text in Morse Code is: {output_code}")
+    if omitted_count > 0:
+        print(f"{omitted_count} characters were omitted.")
+    cont_answer = input("Again (y/n)? ").lower()
+    cont = cont_answer == 'y'
